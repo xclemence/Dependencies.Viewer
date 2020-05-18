455041d6524a97f7566eaffb894ea98958aff52a
Xavier CLEMENCE <xavier.clemence@gmail.com>
2020-05-18 18:33:07 +0200

    Prepare release V2

---
+++ src/Tests/Dependencies.Viewer.UnitTests/ReferenceModelExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
+using Dependencies.Viewer.UnitTests.DataProviders;
+using Dependencies.Viewer.UnitTests.Extensions;
+using Dependencies.Viewer.Wpf.Controls.Extensions;
+using Dependencies.Viewer.Wpf.Controls.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dependencies.Viewer.UnitTests
+{
+
+    [TestClass, ExcludeFromCodeCoverage]
+    public class ReferenceModelExtensionsTests
+    {
+        [TestMethod]
+        public void AssemblyToExchangeModel()
+        {
+            var baseAssembly = AssemblyModelDataProvider.AssemblyTestV4;
+
+            var (assembly, dependencies) = baseAssembly.ToExchangeModel();
+
+            Assert.That.DeepEqual(AssemblyExchangeDataProvider.AssemblyTestV4, assembly);
+            Assert.AreEqual(0, dependencies.Count);
+        }
+    }
+}
